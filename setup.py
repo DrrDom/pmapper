b13cd88510a2fa2ca935bad37c7e12e8ed5de7d6
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2019-08-21 16:33:07 +0200

    Switch version to 0.2

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 from os import path
+import pmapper
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -7,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="pmapper",
-    version="0.2",
+    version=pmapper.__version__,
     author="Pavel Polishchuk",
     author_email="pavel_polishchuk@ukr.net",
     description="pmapper: 3D pharmacophore hashes and fingerprints",
